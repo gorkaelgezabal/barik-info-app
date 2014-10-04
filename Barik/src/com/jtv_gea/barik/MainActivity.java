e1224085f47b6ba609cc642762ec036fbbaa825e
gorkaelgezabal <gorkaelgezabal@gmail.com>
2014-10-04 11:25:33 +0000

    Entra hasta la pantalla de saldo

--- Barik/src/com/jtv_gea/barik/MainActivity.java
+++ Barik/src/com/jtv_gea/barik/MainActivity.java
@@ -5,10 +5,13 @@ import com.jtv_gea.barik.R;
 import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.ConsoleMessage;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
@@ -45,34 +48,24 @@ public class MainActivity extends ActionBarActivity {
 	{
 	    @JavascriptInterface
 	    @SuppressWarnings("unused")
-	    public void processHTML(String html)
+	    public void processHTML(String saldo)
 	    {
 	    	
-	    	if(cont == 1){
-	    		final WebView browser = (WebView)findViewById(R.id.browser);
-	    		
-	    		browser.post(new Runnable() {
-	    		    @Override
-	    		    public void run() {
-	    		    	browser.loadUrl("javascript:getElementById('username').value = 'gorkaelgezabal@gmail.com');");
-	    	    		browser.loadUrl("javascript:getElementById('it1').value = 'contraseña';");
-	    		    	browser.loadUrl("javascript:getElementById('enter').click();");
-	    		    }
-	    		});
-	    		
-	    	}
-	    	cont++;
-	        //System.out.println(html);
+	    	
+	        System.out.println(saldo);
 	    }
 	}
 	
 	
+	
 	public void login(View view){
-		final WebView browser = (WebView)findViewById(R.id.browser);
+		WebView browser = (WebView)findViewById(R.id.browser);
 		
 		/* JavaScript must be enabled if you want it to work, obviously */
 		browser.getSettings().setJavaScriptEnabled(true);
-
+		browser.setWebChromeClient(new WebChromeClient());
+		browser.getSettings().setDomStorageEnabled(true);
+		browser.getSettings().setSaveFormData(false);
 		/* Register a new JavaScript interface called HTMLOUT */
 		browser.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
 
@@ -82,9 +75,41 @@ public class MainActivity extends ActionBarActivity {
 		    public void onPageFinished(WebView view, String url)
 		    {
 		        /* This call inject JavaScript into the page which just finished loading. */
-		        browser.loadUrl("javascript:window.HTMLOUT.processHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
+		        
+		        
+//		        	browser.loadUrl("javascript:window.HTMLOUT.processHTML('<head>'+document.getElementsByTagName('html')[0].innerHTML+'</head>');");
+
+		    	if(cont == 1){//Login
+		    		String javaScript = "(function(){ " +
+		    		        "document.getElementById('username::content').value = 'gorkaelgezabal@gmail.com'; " +
+		    		        "document.getElementById('it1::content').value = '3kIwOwA7'; " +
+		    		        "document.getElementById('enter').click(); " +
+		    		        "})()";
+//					view.loadUrl("javascript:document.getElementById(\"username::content\").innerHTML = \"gorka\";");
+		    		view.loadUrl("javascript: "+javaScript);
+
+		    	}
+		    	else if (cont ==3){//Pagina principal
+		    		//pt1:j_id_id14:1:j_id_id23
+		    		String javaScript = "(function(){ " +
+		    		        "document.getElementById('pt1:j_id_id14:1:j_id_id23').click(); " +
+		    		        "})()";
+//					view.loadUrl("javascript:document.getElementById(\"username::content\").innerHTML = \"gorka\";");
+		    		view.loadUrl("javascript: "+javaScript);
+		    	}
+		    	else if(cont == 4){//Titulos validos
+		    		
+		    		//Aqui no entra porque creo que al hacer click en titulos validos hace una llamada ajax, y por eso no salta OnPageFinished
+		    		view.loadUrl("javascript:window.HTMLOUT.processHTML(document.getElementById('pt1:j_id_id36:subform:j_id_id23pc2::db').getElementsByClassName('xxf')[1].innerHTML);");
+		    	}
+		    	cont++;	
+		    	
 		    }
+		    
+		    
+		    
 		});
 		browser.loadUrl("https://barikweb.cotrabi.com/sagb/faces/Login.jspx");
+		
 	}
 }
